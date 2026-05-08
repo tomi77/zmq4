0de049305fa266f2c268b9895124e5d46a6b65a0
Tomasz Z Rup <tomasz.rup@gmail.com>
2026-05-08 19:17:01 +0200

    feat(F5a): add socket-layer error sentinels

---
+++ errors.go
@@ -0,0 +1,12 @@
+package zmq4
+
+import "errors"
+
+var (
+	ErrClosed           = errors.New("zmq4: socket closed")
+	ErrState            = errors.New("zmq4: operation out of sequence")
+	ErrNoRoute          = errors.New("zmq4: no route to peer")
+	ErrIncompatiblePeer = errors.New("zmq4: incompatible peer socket type")
+	ErrSecurityMismatch = errors.New("zmq4: security option not valid for this role")
+	ErrNoIdentity       = errors.New("zmq4: ROUTER Send requires non-empty msg[0]")
+)
